7da9817c8013d88454320deee96548b73cad797a
Yukitte <80960360+Yukitte@users.noreply.github.com>
2023-10-20 10:36:28 -0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-float conv(float altura , int peso){
-    // float altMet = altura /100;
-    return (peso/pow(altura,2));
+void conv(){
+    float raio ,pi =3.1414592;
+
+    printf("Digite o Raio ");
+    scanf("%f",&raio);
+    
+    float v = (4*pi * pow(raio,3))/3;
+    printf("Volume da esfera :%.1f",v);
 }
 
 int main (){
-    float altura, imc;
-    int peso;
-
-    printf("Digite o peso ");
-    scanf("%d",&peso);
-
-    printf("Digite a Altura ");
-    scanf("%f",&altura);
-
-    imc = conv(altura,peso);
-
-    printf("imc: %.2f",imc);
+    conv();
 
 return 0;
 }
